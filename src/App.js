029f6800a79e5cc56dd27ded1f5b9606561bfe96
jm-nunez <84870508+jm-nunez@users.noreply.github.com>
2021-05-26 16:35:20 -0400

    Creación de Rutas
    Creación de Rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Contacto from './paginas/Contacto';
+import Inicio from './paginas/Inicio';
+import Nosotros from './paginas/Nosotros';
 
 function App() {
   return (
+    <Router>
     <div className="container">
-     <h1>todo ok!</h1>
+    <div className="btn-group"> 
+    <Link to="/" className="btn">Inicio</Link>
+    <Link to="/Nosotros" className="btn">Nosotros</Link>
+    <Link to="/Contacto" className="btn">Contacto</Link>
     </div>
+     <hr/>
+     <Switch>
+      <Route path="/contacto">
+        <Contacto/>
+      </Route>
+      <Route path="/nosotros">
+        <Nosotros/>
+      </Route>
+      <Route path="/">
+        <Inicio/>
+      </Route>
+     </Switch>
+    </div>
+    </Router>
   );
 }
 
